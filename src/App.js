a0ea358d3e94414ba95b11bffb2bd5c96cebc342
Athul Rajagopalan P <athulrajagopalan88@gmail.com>
2023-12-30 14:56:14 +0530

    app.jsx edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import './App.css';
 import Header from "./components/Header";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input,setInput] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(initialState)
   const [editTodo, setEditTodo] = useState(null)
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos])
   return (
     <div className="container">
       <div className="app-wrapper">
